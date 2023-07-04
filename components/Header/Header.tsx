df96a63a080e1c9b289f667804bdfb4fffe15887
AlanKh98 <106619328+AlanKh98@users.noreply.github.com>
2023-07-04 21:47:54 +0300

    I Alan Deployeeeed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,7 +14,8 @@ function header() {
                 <div className={styles.pages}>
                     <ul>
                         <li className={`${styles.headerPages} ${styles.headerPagesActive}`} onClick={() => router.push('/')}>Home</li>
-                        <li className={styles.headerPagesList} onClick={() => router.push('/Products')}>Shop 
+                        <li className={styles.headerPagesList} >
+                            <a onClick={() => router.push('/Products')}>Shop</a> 
                             <svg xmlns="http://www.w3.org/2000/svg"     height="24" width="24" className={styles.shop} fill="currentColor" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                             </svg>
